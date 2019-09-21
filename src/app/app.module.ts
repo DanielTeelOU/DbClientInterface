4d849b6d5f95da22fa583b0eec3b7a451938e8d8
Danny Teel <teelman1996@gmail.com>
2019-09-20 23:43:16 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { 
+  MatTableModule, 
+  MatDialogModule, 
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSortModule,
+  MatPaginator,
+  MatPaginatorModule
+} from '@angular/material';
+
+import { DialogBoxxComponent } from './dialog-boxx/dialog-boxx.component';
+import { TableFilteringComponent } from './table-filtering/table-filtering.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogBoxxComponent,
+    TableFilteringComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSortModule,
+    MatPaginatorModule
+  ],
+  entryComponents: [
+    DialogBoxxComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
